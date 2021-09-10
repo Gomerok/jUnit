aff3f00904da67490ea5652033c5f7fc3ba79a59
Gomerov Daniil <danila.gomerov@gmail.com>
2021-09-11 01:29:27 +0300

    Homework7

--- by.academy.junit/src/main/java/by/academy/junit/Calculator.java
+++ by.academy.junit/src/main/java/by/academy/junit/Calculator.java
@@ -2,10 +2,6 @@ package by.academy.junit;
 
 public class Calculator {
 
-//	4) написать 2 класса, в которых вы напишите тесты на Math библиотеку( любую функцию), объеденить их в Test Suite.
-//	5) добавить CalculatorTestListener, переопределить, метод который вызывается на успешное 
-//	завершение теста и выводит на экран название нашего метода.	
-	
 	public static int getSum(int x, int y) {
 		return x + y;
 	}
